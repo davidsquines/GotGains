14fac9314b9b0e53cfeb5dc6ac4b4cbc6fd6ef89
davidsquines <davidsquines@gmail.com>
2023-01-02 14:01:36 -0800

    COrs fix (2)

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express();
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const corsOptions = {
-    origin:'*', 
+    origin:['https://got-gains.herokuapp.com/', 'http://localhost:3000'] , 
     credentials:true,  
     optionSuccessStatus:200,
 }
